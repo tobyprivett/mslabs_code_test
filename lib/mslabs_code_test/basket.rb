e3529a98c8c46a5423e2c00a2a9080b024ec07cc
Toby Privett <toby@snaplab.co.uk>
2016-04-23 10:39:41 +0100

    Setup a test for an empty basket

--- lib/mslabs_code_test/basket.rb
+++ lib/mslabs_code_test/basket.rb
@@ -1,7 +1,18 @@
 module MslabsCodeTest
   class Basket
+    attr_accessor :items
+
     def initialize
       self
+      @items = []
+    end
+
+    def add_products(items)
+      @items << items
+    end
+
+    def calculate_total
+     0.00
     end
   end
 end
--- spec/lib/basket_spec.rb
+++ spec/lib/basket_spec.rb
@@ -5,4 +5,20 @@ describe MslabsCodeTest::Basket do
   it "initializes a Basket" do
     expect(MslabsCodeTest::Basket.new).to be_a(MslabsCodeTest::Basket)
   end
+
+  describe "making a purchase" do
+    let!(:basket) { MslabsCodeTest::Basket.new }
+
+    before { basket.add_products(products) }
+
+    subject { basket.calculate_total }
+
+    context "with no products" do
+      let(:products) { [] }
+
+      it "totals 0.00" do
+        expect(subject).to eql(0.00)
+      end
+    end
+  end
 end
